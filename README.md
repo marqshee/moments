d0438a873544e718a28e86cf88d432d93e37b3b1
Sheena Marquez <sheena@sheenas-air.lan>
2020-08-16 15:32:00 -0700

    updated README and working lambda function

--- README.md
+++ README.md
@@ -1,9 +1,44 @@
 # newness-takehome-moments
-Take home assignment.
-Due: 8/17/2020
+Take home assignment. Due: 8/17/2020
 
-### Publishing a AWS Layer
+### Moments
+Livestreaming is a great way for creators to engage with their viewers, but the long-form content is not always ideal for viewers to consume after the stream ends. Creators want to share the delightful moments that happen in a lives tream with their fans. To aid them, we are building a new product called Moments as a novel and catchier way for creators to generate hype for their upcoming lives treams by cross-promoting to their other social platforms. Each moment is a short segment of the live stream video, clipped by our creators and their moderator team.
+
+### 'createMoment' API
+The 'createMoment' API is an AWS service that will:
+ *  processes m3u8 file based on quality
+ *  creates a new mp4 file based on the start and end times using ffmpeg
+ *  uploads the moment to a S3 bucket named newness-takehome-moments
+
+### AWS Service Architecture
+The 'createMoment' APIs is built using AWS Lambda Integration which is a FAAS product. Lambda service allows for us to quickly prototype and or handle manipulation of data without having to worry about long term maintenance of a fleet service like EC2. In this case, 'createMoment' pulls from S3, parses a playlist for duration, pulls relevant files from S3 and uses FFMPEG to process the individual files into a MP4 which is then stored back into S3. There is no need to remember state for this particular API and because of this Lambda provides the perfect environment. Because they are stateless, AWS can execute the function easily on an open agent in the fleet. This allows the API to scale up with usage easily. 
+
+This current version of 'createMoment' API is using the traditional Lambda Integration vs Lambada Proxy Integration. While the newer Lambda Proxy Integration is optimized for AWS services, the traditional Lambda Integration allows for more customizable payloads and less coupling to the AWS system if we needed to migrate to another system in the future.
+
+The API is currently using an FFMPEG and m3u8-parser layer to allow the NodeJS environment access to helper modules which allow us to create a moment from an existing VOD based on user desired start and end times. The FFMPEG layer allows us to convert the .ts files into a playable mp4.
+
+The AWS API Gatway is the entry point to accessing the 'createMoment' Lambada function. Since we are using the traditional Lambda Integration, the API Gateway does require additional setup of integration responses between the gateway and Lambda.
+
+## How to use via Postman
+ * Download [Postman](https://www.postman.com)
+ * POST to the [API url](https://e9hvdq2xpa.execute-api.us-west-2.amazonaws.com/default/createMoment)
+ * In the Request Body:
+ ```
+ {
+  "id": "081957680644",
+  "s3_bucket": "newness-takehome-livestream",
+  "s3_key_prefix": "081957680644/W2qj91sl5VDK/2020-08-03T16-21-25.146Z/EyEKX6dRDP81",
+  "resolution": "720p60",
+  "start_time": 60,
+  "end_time": 120
+}
 ```
-aws lambda publish-layer-version --layer-name my-layer --description "My layer" --license-info "MIT" \
---content S3Bucket=lambda-layers-us-east-2-123456789012,S3Key=layer.zip --compatible-runtimes python3.6 python3.7
+ * Please note, the 'createMoment' API requires an API key and since it is in staging AWS credentials. Log into AWS console, and view the 'createMoment' API in the AWS API Gateway (us-west-2).
+ * Send the request.
+ * On success you'll see:
+ ```
+ {
+    "statusCode": 200,
+    "body": "Moment Created!"
+}
 ```
--- index.js
+++ index.js
@@ -1,91 +1,138 @@
-const AWS = require('aws-sdk');
-const child = require('child_process');
-const file = require('fs');
-const m3u8Parser = require('m3u8-parser');
-const s3 = new AWS.S3({region: 'us-west-1'});
+const AWS = require("aws-sdk");
+const child = require("child_process");
+const file = require("fs");
+const m3u8Parser = require("m3u8-parser");
+const s3 = new AWS.S3({ region: "us-west-1" });
 
+/**
+ * AWS Lambda service:
+ *  - processes m3u8 file based on quality
+ *  - creates a new mp4 file based on the start and end times using ffmpeg
+ *  - uploads the moment to a S3 bucket named newness-takehome-moments
+ * @param {object} event
+ * @returns {object}
+ */
 exports.handler = async (event) => {
-  
+  try {
     const endTime = event.end_time;
     const s3Bucket = event.s3_bucket;
     const s3KeyFullPrefix = `${event.s3_key_prefix}/hls/${event.resolution}`;
     const startTime = event.start_time;
+    const s3MomentBucket = "newness-takehome-moments";
 
-    const s3MomentBucket = 'newness-takehome-moments'; 
-    const params = {Bucket: s3Bucket, Key: `${s3KeyFullPrefix}/playlist.m3u8`};
-    const response = await s3.getObject(params).promise();
-    const playlist = response.Body.toString('utf-8');
-    const momentsTsFiles = playlistMomentParser(playlist, startTime, endTime);
+    // Retrieves HLS m3u8 playlist based on quality
+    const params = {
+      Bucket: s3Bucket,
+      Key: `${s3KeyFullPrefix}/playlist.m3u8`,
+    };
+    const res = await s3.getObject(params).promise();
+    const playlist = res.Body.toString("utf-8");
+    const segments = playlistMomentParser(playlist, startTime, endTime);
 
-    await getMomentTsFilesFromS3(momentsTsFiles, s3Bucket, s3KeyFullPrefix);
-    let command = "cat /tmp/*.ts >/tmp/stitch.txt";
-    child.execSync(command);
-    
-    let ffmepgCmd = `ffmpeg -ss ${momentsTsFiles.startOffset} -t ${momentsTsFiles.endOffset} -i /tmp/stitch.txt -acodec copy -vcodec copy /tmp/moment.mp4`;
-    child.execSync(ffmepgCmd);
-    
-    const mp4 = file.readFileSync('/tmp/moment.mp4');
+    let files = await getMomentFilesFromS3(segments, s3Bucket, s3KeyFullPrefix);
+    let inputs = files.join("|");
+    let duration = endTime - startTime;
+    child.execSync(
+      `ffmpeg -i "concat:${inputs}" -ss ${segments.startOffset} -t ${duration} -acodec copy -vcodec copy /tmp/moment.mp4`
+    );
+
+    const mp4 = file.readFileSync("/tmp/moment.mp4");
     var date = new Date().toISOString();
     const putParams = {
       Bucket: s3MomentBucket,
-      Key: `${s3KeyFullPrefix}/moment-${date}.mp4`, 
+      Key: `${s3KeyFullPrefix}/moment-${date}.mp4`,
       Body: mp4,
-    }
+    };
 
     await s3.putObject(putParams).promise();
-    child.execSync('rm /tmp/*.ts; rm /tmp/stitch.txt; rm /tmp/moment.mp4');
+
+    child.execSync("rm /tmp/*.ts; rm /tmp/stitch.txt; rm /tmp/moment.mp4");
+
+    const response = {
+      statusCode: 200,
+      body: "Moment Created!",
+    };
+    return response;
+  } catch (err) {
+    const response = {
+      statusCode: err.statusCode,
+      body: err.message,
+    };
+    throw new Error(JSON.stringify(response));
+  }
 };
 
+/**
+ * Parses m3u8 playlist for ts files within  given start and end time ranges and
+ * calculates start and end time offsets.
+ *
+ * @param {string} playlist
+ * @param {number} startTime
+ * @param {number} endTime
+ *
+ * @returns {object}
+ */
 function playlistMomentParser(playlist, startTime, endTime) {
   const parser = new m3u8Parser.Parser();
   let duration = 0;
   let hasMomentStarted = false;
-  let hasMomentEnded = false;
-  let momentStartingTs = null;
-  let momentEndingTs = null;
-  let momentsTsFiles = [];
+  let files = [];
   let startOffset = null;
-  let endOffset = null;
 
   parser.push(playlist);
   parser.end();
   const segments = parser.manifest.segments;
 
   for (let segment of segments) {
-      duration = duration + segment.duration;
-      if (duration > startTime && !hasMomentStarted) {
-          startOffset = duration - startTime;
-          momentStartingTs = segment.uri;
-          hasMomentStarted = true;
-      }
-      if (hasMomentStarted && !hasMomentEnded) {
-          momentsTsFiles.push(segment.uri);
-      }
-      if (duration > endTime && !hasMomentEnded) {
-        endOffset = duration - endTime;
-        momentEndingTs = segment.uri;
-        hasMomentEnded = true;
+    duration += segment.duration;
+
+    if (duration > startTime && !hasMomentStarted) {
+      startOffset = startTime - (duration - segment.duration);
+      hasMomentStarted = true;
+    }
+
+    if (hasMomentStarted) {
+      files.push(segment.uri);
+    }
+
+    if (duration > endTime) {
+      break;
     }
   }
-  return {momentsTsFiles, startOffset, endOffset};
+  return { files, startOffset };
 }
 
-function getMomentTsFilesFromS3(momentsTsFiles, s3Bucket, s3KeyFullPrefix) {
-    var promises = [];
+/**
+ * Retrieves files from S3 and saves them to /tmp.
+ * @param {array} segments 
+ * @param {string} s3Bucket 
+ * @param {string} s3KeyFullPrefix 
+ * 
+ * @returns {object}
+ */
+function getMomentFilesFromS3(segments, s3Bucket, s3KeyFullPrefix) {
+  var promises = [];
 
-    for (let tsFile of momentsTsFiles.momentsTsFiles) {
-        promises.push( new Promise((resolve, reject) => {
-            const destination = `/tmp/${tsFile}`;
-            const fileStream = file.createWriteStream(destination);
-            const params = {Bucket: s3Bucket, Key: `${s3KeyFullPrefix}/${tsFile}`};
-      
-            const s3Stream = s3.getObject(params).createReadStream();
-            s3Stream.on('error', reject);
-            fileStream.on('error', reject);
-            fileStream.on('close', () => { resolve(destination);});
-            s3Stream.pipe(fileStream);
-          }));
-    }
+  for (let tsFile of segments.files) {
+    promises.push(
+      new Promise((resolve, reject) => {
+        let destination = `/tmp/${tsFile}`;
+        let fileStream = file.createWriteStream(destination);
+        const params = {
+          Bucket: s3Bucket,
+          Key: `${s3KeyFullPrefix}/${tsFile}`,
+        };
 
-    return Promise.all(promises);
+        let s3Stream = s3.getObject(params).createReadStream();
+        s3Stream.on("error", reject);
+        fileStream.on("error", reject);
+        fileStream.on("finish", () => {
+          resolve(destination);
+        });
+        s3Stream.pipe(fileStream);
+      })
+    );
+  }
+
+  return Promise.all(promises);
 }
